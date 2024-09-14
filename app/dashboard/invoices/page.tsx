0360c307b68901abd7a9e4db3dea1355086b9380
aaron moroney <aaron.createdesign@gmail.com>
2024-09-14 20:25:48 +0100

    Learning next.js chapter 11: Adding search and pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-    return <p>Inovices Page</p>;
+import { Suspense } from 'react';
+
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+
+// https://nextjs.org/learn/dashboard-app/adding-search-and-pagination
+interface InvoicePageProps {
+  searchParams?: {
+    query?: string
+    page?: string 
   }
+}
+
+// if we search in the field, the URL will update - the data will be fetched, and our queried term will be returned
+export default async function Page({ searchParams }: InvoicePageProps) {
+  const query = searchParams?.query || '';
+  const currentPage =  Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+   
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        {/* search is a client component -> useSearchParams, as a general rule: read params from client, useSearchParams() */}
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        {/* server component */}
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,37 @@
 'use client';
 
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 
+//https://nextjs.org/learn/dashboard-app/adding-search-and-pagination
 export default function Search({ placeholder }: { placeholder: string }) {
+ 
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace }= useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    // this log illustrates the on every key stroke, querying DB on every keystroke
+    console.log(`Searching... ${term}`);
+
+    // we'll use a debouncing to limit this 
+    // how it works 
+    // ---------------
+    // 1. Trigger-event: starts the debounce timer
+    // 2. Wait: if a new event occurs before the timer expires, a new timer is set
+    // 3. Execution: if the timer reaches the end, the debounced function is executed
+    
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    term ? params.set('query', term) : params.delete('query');
+    // pathname is the current path, in our case -/dashboard/invoices
+    // params.toString() translates the term into URL friendly format
+    // ^ e.g /dashboard/invoices?query=lee if the term is 'lee'
+    // URL is updated without reloading the page -> next.js client side navigation
+    replace(`${pathname}?${params.toString()}`)
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +40,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
+        // this is an uncontrolled approach, as we are not using state, the input manages it's own state
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>

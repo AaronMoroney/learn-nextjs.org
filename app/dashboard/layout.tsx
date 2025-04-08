78b01d8d7ed2c75807355c45833f559496395525
aaron moroney <aaron.createdesign@gmail.com>
2025-04-08 18:44:41 +0100

    Chapter 10: Partial pre-rendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+//Add this to use PPR - still, experimental.
+
+//export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
